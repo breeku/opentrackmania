9c7af9bd3c5d867c2cd18c275dd5f1fca7f024f7
breeku <bregfi@hotmail.com>
2020-08-08 17:03:57 +0300

    ?

--- server/index.ts
+++ server/index.ts
@@ -17,16 +17,14 @@ app.use('/api/totds', totdRouter)
 app.use('/api/tracks', trackRouter)
 app.use('/api/seasons', seasonRouter)
 
-// Serve any static files
-app.use(express.static(path.join(__dirname, '../client/build')))
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../client/build')))
 
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-})
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    })
+}
 
-const PORT = process.env.PORT || 8080
-
-app.listen(PORT)
-
-console.log('Listening on port ' + PORT)
+app.listen(process.env.PORT || 8080)
