9493d01cc0b988394ea2f04455580a4df577f941
breeku <bregfi@hotmail.com>
2020-08-13 17:41:24 +0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -19,14 +19,4 @@ app.use('/api/tracks', trackRouter)
 app.use('/api/seasons', seasonRouter)
 app.use('/api/players', playerRouter)
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')))
-
-    // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    })
-}
-
 app.listen(process.env.PORT || 8080)
